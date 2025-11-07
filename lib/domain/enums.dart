21389398bb01c514ab6f52c1798bca1bcbe5b32d
Sotheara Kon <sothearakon73@gmail.com>
2025-11-08 00:23:22 +0700

    update enum

--- lib/domain/enums.dart
+++ lib/domain/enums.dart
@@ -1,4 +1,4 @@
-enum Role { doctor, nurse, admin, receptionist}
+enum Role {doctor, nurse}
 enum Gender {male, female, other}
 enum DayOfWeek {monday, tuesday, wednesday, thursday, friday, saturday, sunday}
 enum AppointmentStatus {scheduled, completed, cancel}
